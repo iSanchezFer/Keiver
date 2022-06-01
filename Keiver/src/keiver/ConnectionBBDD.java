5c9eba49fdbc6061c869de4e187e4be154fcf3ff
BertexX <96374948+BertexX@users.noreply.github.com>
2022-06-01 23:43:49 +0200

    Add files via upload

--- Keiver/src/keiver/ConnectionBBDD.java
+++ Keiver/src/keiver/ConnectionBBDD.java
@@ -17,7 +17,6 @@ public class ConnectionBBDD {
 
 			File dataFile = new File("src/keiver/bbdd_data.txt");
 
-			@SuppressWarnings("resource")
 			BufferedReader file = new BufferedReader(new FileReader(dataFile));
 
 			while ((end = file.readLine()) != null) {
@@ -26,10 +25,8 @@ public class ConnectionBBDD {
 			}
 
 		} catch (FileNotFoundException e) {
-			Logs.appendToFile(e);
 			System.err.println("No se ha encontrado el archivo -> " + e.getMessage());
 		} catch (Exception e) {
-			Logs.appendToFile(e);
 			System.err.println("Error imprevisto");
 		}
 
@@ -38,10 +35,10 @@ public class ConnectionBBDD {
 			Connection myConnection = DriverManager.getConnection(data[0], data[1], data[2]);
 
 			System.out.println("Connection OK");
+			
 			return myConnection;
 
 		} catch (Exception e) {
-			Logs.appendToFile(e);
 			System.out.println(e.getMessage());
 			return null;
 		}
